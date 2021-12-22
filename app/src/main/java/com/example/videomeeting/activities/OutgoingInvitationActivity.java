2dbfdae928b824e44af052459223dbca3b142aef
Aman Kumar <amankumar34276@gmail.com>
2021-12-22 14:12:37 +0530

    Sending and receiving Accept, Reject and onCancelled response is successful

--- app/src/main/java/com/example/videomeeting/activities/OutgoingInvitationActivity.java
+++ app/src/main/java/com/example/videomeeting/activities/OutgoingInvitationActivity.java
@@ -2,11 +2,17 @@ package com.example.videomeeting.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 import com.example.videomeeting.utilities.Constants;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
+
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,7 +24,7 @@ import com.example.videomeeting.network.ApiService;
 import com.example.videomeeting.utilities.PreferenceManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-//import com.google.firebase.messaging.Constants;
+
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.json.JSONArray;
@@ -40,14 +46,7 @@ public class OutgoingInvitationActivity extends AppCompatActivity {
 
 
         preferenceManager = new PreferenceManager(getApplicationContext());
-        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
-            @Override
-            public void onComplete(@NonNull Task<String> task) {
-                if(task.isSuccessful() && task.getResult() != null){
-                    inviterToken = task.getResult();
-                }
-            }
-        });
+
 
         ImageView imageMeetingType = findViewById(R.id.imageMeetingType);
 
@@ -75,12 +74,28 @@ public class OutgoingInvitationActivity extends AppCompatActivity {
 
 
         ImageView imageStopInvitation = findViewById(R.id.imageStopInvitation);
-        imageStopInvitation.setOnClickListener(v -> onBackPressed());
+        imageStopInvitation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user != null){
+                    cancelInvitation(user.token);
+                }
+            }
+        });
+
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if(task.isSuccessful() && task.getResult() != null){
+                    inviterToken = task.getResult();
+                    if(meetingType != null && user != null){
+                        initiateMeeting(meetingType, user.token);
+                    }
+                }
+            }
+        });
 
 
-        if(meetingType != null && user != null){
-            initiateMeeting(meetingType, user.token);
-        }
     }
 
     private void initiateMeeting(String meetingType, String receiverToken){
@@ -120,6 +135,10 @@ public class OutgoingInvitationActivity extends AppCompatActivity {
                     if(type.equals(Constants.REMOTE_MSG_INVITATION)){
                         Toast.makeText(OutgoingInvitationActivity.this, "Invitation sent Successfully", Toast.LENGTH_SHORT).show();
                     }
+                    else if(type.equals(Constants.REMOTE_MSG_INVITATION_RESPONSE)){
+                        Toast.makeText(OutgoingInvitationActivity.this, "Invitation Cancelled", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
                 }else{
                     Toast.makeText(OutgoingInvitationActivity.this, response.message(), Toast.LENGTH_SHORT).show();
                     finish();
@@ -135,4 +154,61 @@ public class OutgoingInvitationActivity extends AppCompatActivity {
 
     }
 
+    private void cancelInvitation(String receiverToken){
+        try{
+
+            JSONArray tokens = new JSONArray();
+            tokens.put(receiverToken);
+
+            JSONObject body = new JSONObject();
+            JSONObject data = new JSONObject();
+
+            data.put(Constants.REMOTE_MSG_TYPE, Constants.REMOTE_MSG_INVITATION_RESPONSE);
+            data.put(Constants.REMOTE_MSG_INVITATION_RESPONSE, Constants.REMOTE_MSG_INVITATION_CANCELLED);
+
+            body.put(Constants.REMOTE_MSG_DATA, data);
+            body.put(Constants.REMOTE_MSG_REGISTRATION_IDS, tokens);
+
+            sendRemoteMessage(body.toString(), Constants.REMOTE_MSG_INVITATION_RESPONSE);
+
+        }
+        catch(Exception exception){
+            Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    private BroadcastReceiver invitationResponseReceiver = new BroadcastReceiver(){
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String type = intent.getStringExtra(Constants.REMOTE_MSG_INVITATION_RESPONSE);
+            if(type != null){
+                if(type.equals(Constants.REMOTE_MSG_INVITATION_ACCEPTED)){
+                    Toast.makeText(context, "Invitation Accepted", Toast.LENGTH_SHORT).show();
+                }
+                else if(type.equals(Constants.REMOTE_MSG_INVITATION_REJECTED)){
+                    Toast.makeText(context, "Invitation Rejected", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(
+                invitationResponseReceiver,
+                new IntentFilter(Constants.REMOTE_MSG_INVITATION_RESPONSE)
+        );
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(
+                invitationResponseReceiver
+        );
+    }
 }
--- app/src/main/java/com/example/videomeeting/firebase/MessagingService.java
+++ app/src/main/java/com/example/videomeeting/firebase/MessagingService.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.example.videomeeting.activities.IncomingInvitationActivity;
 import com.example.videomeeting.utilities.Constants;
 import androidx.annotation.NonNull;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -45,8 +46,21 @@ public class MessagingService extends FirebaseMessagingService {
                         Constants.Key_Email,
                         remoteMessage.getData().get(Constants.Key_Email)
                 );
+
+                intent.putExtra(
+                        Constants.REMOTE_MSG_INVITER_TOKEN,
+                        remoteMessage.getData().get(Constants.REMOTE_MSG_INVITER_TOKEN)
+                );
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
+            }
+            else if(type.equals(Constants.REMOTE_MSG_INVITATION_RESPONSE)){
+                Intent intent = new Intent(Constants.REMOTE_MSG_INVITATION_RESPONSE);
+                intent.putExtra(
+                        Constants.REMOTE_MSG_INVITATION_RESPONSE,
+                        remoteMessage.getData().get(Constants.REMOTE_MSG_INVITATION_RESPONSE)
+                );
+                LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
             }
         }
 
